34ee03a1f22767dd48f1157c09ea06ecff71601a
Oreo0925 <yarouhsu@gmail.com>
2018-10-21 14:32:55 +0800

    navigation schematic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,58 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule, MatToolbarModule, MatButtonModule, MatIconModule, MatListModule} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { IotComponent } from './iot/iot.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+import { ThirdPageComponent } from './third-page/third-page.component';
+
+const appRoutes: Routes = [
+  {path: 'first-page' , component: FirstPageComponent},
+  {path: 'second-page' , component: SecondPageComponent},
+  {path: 'third-page' , component: ThirdPageComponent}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IotComponent,
+    FirstPageComponent,
+    SecondPageComponent,
+    ThirdPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    HttpClientModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule
+  ],
+  exports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatNativeDateModule,
+    HttpClientModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
